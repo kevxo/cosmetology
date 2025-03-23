89f91ed594a19afdd232de1f4ce2a464dd62c25f
Kevin Cuadros <kev040798@gmail.com>
2025-03-22 20:57:24 -0400

    Add helper methods for hash password verifying password, and token expiration

---
+++ backend/users-service/auth.py
@@ -0,0 +1,23 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+import os
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return pwd_context.verify_password(password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
