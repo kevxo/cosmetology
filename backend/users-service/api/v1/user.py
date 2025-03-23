d823e24a4dfb7af5053b53fab69e05a6e8605e7f
Kevin Cuadros <kev040798@gmail.com>
2025-03-22 20:56:24 -0400

    Create register user endpoint

--- backend/users-service/api/v1/user.py
+++ backend/users-service/api/v1/user.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from pydantic_schemas.user import UserCreate, UserResponse
+from api.db_helpers.user import create_user
 
 router = APIRouter()
 
-@router.get('/api/v1/users')
-def get_all_users():
-    return {"msg": "Hello World!"}
+@router.post('/api/v1/register_user', status_code=status.HTTP_201_CREATED, response_model=UserResponse)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    user = create_user(user, db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Bad Request. Please try again."
+        )
+
+    return user
